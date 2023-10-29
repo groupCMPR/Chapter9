bca82c132cd28036e834c7bacdde566e491660ef
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-28 20:48:38 -0700

    Create Queen.h

---
+++ Queen.h
@@ -0,0 +1,26 @@
+#pragma 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Queen {
+private:
+	vector <vector<char>> board;
+	int board_size;
+	int initial_column;
+
+public:
+	//default constructor
+	Queen(const int&, const int&);
+
+	//mutator
+	void solve_Queen(int, int);
+
+	//accessor
+	bool is_Correct() const;
+	int get_Size() const;
+	char get_Char(const int&, const int&) const;
+
+	//friend
+	friend ostream& operator<<(ostream& out, const Queen& obj);
+};
