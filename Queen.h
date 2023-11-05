9300f47da748ae27597e64448bd0224aa64fff1e
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-11-04 20:36:00 -0700

    Update Queen.h

--- Queen.h
+++ Queen.h
@@ -1,7 +1,3 @@
-//Names: Neidy Malaga 
-//Date: 10/28/2023
-//Description: Header file for class Queen
-
 #pragma 
 #include <iostream>
 #include <vector>
@@ -10,21 +6,23 @@ using namespace std;
 class Queen {
 private:
 	vector <vector<char>> board;
+	bool backtrack_finish;
 	int board_size;
-	int initial_column;
 
 public:
 	//default constructor
 	Queen(const int&, const int&);
 
-	//mutator
-	void solve_Queen(int, int);
+
+	//mutators
+	void solve_Queens(int row);
 
 	//accessor
 	bool is_Correct() const;
 	int get_Size() const;
 	char get_Char(const int&, const int&) const;
+	bool queen_Check(const int&, const int&) const;
 
 	//friend
-	friend ostream& operator<<(ostream& out, const Queen& obj);
+	friend ostream& operator<<(ostream&, const Queen&);
 };
