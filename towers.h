bbafb017b040ddfa73c35fe9bf9a3af86f957447
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-11-06 16:21:05 -0800

    Update towers.h

--- towers.h
+++ towers.h
@@ -1,4 +1,7 @@
-//Alexis and Angie
+//Names: Alexis Martinez & Angie Campos 
+//Date: 10/28/2023
+//Description: Derived header file for class tower
+
 #pragma once
 #include "peg.h"
 
@@ -6,7 +9,7 @@ class towers
 {
 private:
 	peg pegs[3];
-	int move; 
+	int move;
 
 public:
 	//default game start
@@ -19,10 +22,8 @@ public:
 	void displayAllTowers(int) const;
 	//checks for a win 
 	bool winCheck(int);
-
+	//get the move
 	int getMove();
-
+	//display solved game
 	void solveGame(int numOfRings, char fromPeg, char toPeg, char auxPeg);
-
 };
-
