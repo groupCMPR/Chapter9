ce464b99495845e2b993dd140ae471415ce76309
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-11-06 16:21:47 -0800

    Update peg.h

--- peg.h
+++ peg.h
@@ -1,4 +1,7 @@
-//Alexis and Angie
+//Names: Alexis Martinez & Angie Campos 
+//Date: 10/28/2023
+//Description: Header file for class peg
+
 #pragma once
 #include <iostream>
 #include <stack>
@@ -8,7 +11,6 @@ class peg
 {
 private:
 	stack<int> rings;
-
 
 public:
 	peg();
@@ -24,7 +26,4 @@ public:
 	int pegSize() const;
 	// returns the stack
 	stack<int> getStack() const;
-
-
 };
-
